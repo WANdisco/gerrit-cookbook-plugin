eea84e7e07ecf6ebb70ea5a6b0cde67f5a5576af
Marco Miller <marco.miller@ericsson.com>
2015-10-16 13:46:14 -0400

    Add revision http(d) test to CookbookIT for further example purposes
    Depends on gerrit-acceptance-framework change id I78bda7.
    
    Change-Id: I3a1973f87cab38d5bd6bf8f9e09f763753e7b1d9

--- src/test/java/com/googlesource/gerrit/plugins/cookbook/CookbookIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/cookbook/CookbookIT.java
@@ -16,17 +16,25 @@ package com.googlesource.gerrit.plugins.cookbook;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import com.google.gerrit.acceptance.NoHttpd;
 import com.google.gerrit.acceptance.PluginDaemonTest;
+import com.google.gerrit.acceptance.RestResponse;
 
 import org.junit.Test;
 
-@NoHttpd
 public class CookbookIT extends PluginDaemonTest {
 
   @Test
   public void printTest() throws Exception {
     assertThat(sshSession.exec("cookbook print")).isEqualTo("Hello world!\n");
     assertThat(sshSession.hasError()).isFalse();
+  }
+
+  @Test
+  public void revisionTest() throws Exception {
+    createChange();
+    RestResponse response =
+        adminSession.post("/changes/1/revisions/1/cookbook~hello-revision");
+    assertThat(response.getEntityContent())
+        .contains("Hello admin from change 1, patch set 1!");
   }
 }
